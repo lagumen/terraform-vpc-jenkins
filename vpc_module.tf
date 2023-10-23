b3f0bec1797c36465b39949e51bcbad1cf1f8115
Tony Lagumen <tlagumen@gmail.com>
2023-10-23 09:02:28 -0500

    added README file

---
+++ vpc_module.tf
@@ -0,0 +1,54 @@
+# Create VPC Terraform Module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  # VPC Basic Details
+  name            = "vpc-dev"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.2.0/24", "10.0.4.0/24"]
+
+  # Database Subnets
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
+  database_subnets                   = ["10.0.6.0/24", "10.0.8.0/24"]
+
+  #create_database_nat_gateway_route = true
+  #create_database_internet_gateway_route = true
+
+  # NAT Gateways - Outbound Communication
+  enable_nat_gateway = true
+  single_nat_gateway = false
+
+  # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
+  tags = {
+    Owner       = "lagumen"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-dev"
+  }
+  # Instances launched into the Public subnet should be assigned a public IP address.
+  map_public_ip_on_launch = true
+}
+
+
+
